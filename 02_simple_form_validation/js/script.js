3d712c929de43a543fe1bf8db58683c4ea24fade
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-09-29 20:35:25 +0200

    init

---
+++ 02_simple_form_validation/js/script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("#contactForm");
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
+const lastName = document.querySelector("#lastName");
+const lastNameError = document.querySelector("#lastNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
---
+++ 03_front_end_for_strapi/js/utils/storage.js
@@ -0,0 +1,12 @@
+
+// export saveToken
+
+// export get getToken
+
+// export saveUser
+
+// get user name
+
+// save to storage
+
+// get from storage
